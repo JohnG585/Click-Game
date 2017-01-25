64b4238ad1b611dd7a76e60d10fa9e21d22c114b
JohnG585 <JGallagher585@Gmail.com>
2017-01-25 10:53:50 -0700

    Click Game
    Can't get it to show messages in "p". Investigating what needs to
    change.

---
+++ app.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  var leftOffset =0;
+  var moveHeading=function() {
+    $("#heading").offset({left: leftOffset});
+    leftOffset++;
+    if (leftOffset > 200) {
+      leftOffset=0
+    }
+  };
+})
+setInterval(moveHeading, 30);
